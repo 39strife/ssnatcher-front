bc7ce3cf320f64f1cdf8d689ac0f514658afaa2e
Alek Angelov <alekangelov@DESKTOP-FEFO6TP.localdomain>
2020-08-17 11:40:58 +0200

    snatch front profile changes and typo

--- pages/profile/[slug].jsx
+++ pages/profile/[slug].jsx
@@ -3,7 +3,6 @@ import Layout from "../../lib/Layout";
 import { getSingleProfile } from "../../lib/requests";
 import { STORAGE_URL } from "../../lib/hooks/useRequest";
 export default function SingleProfile({ user }) {
-  console.log(user);
   return (
     <Layout>
       <div className="profile-page">
@@ -18,9 +17,35 @@ export default function SingleProfile({ user }) {
                 <img src={STORAGE_URL + user.profile.avatar} />
               </div>
               <div className="col-md-8 description">
-                <h1>{user.username}</h1>
+                <div className="row align-center">
+                  <div className="col-md-6">
+                    <h1>{user.username}</h1>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="profile-page_socials">
+                      {Boolean(user.profile.socials) &&
+                        user.profile.socials.map((e) => {
+                          return <span>{e.value}</span>;
+                        })}
+                    </div>
+                  </div>
+                </div>
                 <p>{user.profile.description}</p>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="wrapper p-y-10">
+          <div className="row">
+            <div className="col-md-12">
+              <h2>Combos</h2>
+            </div>
+          </div>
+        </div>
+        <div className="wrapper p-y-10">
+          <div className="row">
+            <div className="col-md-12">
+              <h2>Posts</h2>
             </div>
           </div>
         </div>
